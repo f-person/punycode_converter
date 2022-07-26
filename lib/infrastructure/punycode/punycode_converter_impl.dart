8142e552c768e29fde9994b446cb383c0c5fea76
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2022-07-26 19:29:29 +0400

    feat: Use a proper algorithm

--- lib/infrastructure/punycode/punycode_converter_impl.dart
+++ lib/infrastructure/punycode/punycode_converter_impl.dart
@@ -1,17 +1,17 @@
+import 'package:basic_utils/basic_utils.dart';
 import 'package:fpdart/fpdart.dart';
-import 'package:punycode/punycode.dart';
 import 'package:punycode_converter/domain/punycode/i_punycode_converter.dart';
 
 class PunycodeConverterImpl implements IPunycodeConverter {
   @override
   String convertTextToPunycode(String text) {
-    return punycodeEncode(text, upperCase: true);
+    return DomainUtils.toIDN(text);
   }
 
   @override
   Either<PunycodeConversionFailure, String> convertPunycodeToText(String punycode) {
     try {
-      final text = punycodeDecode(punycode);
+      final text = DomainUtils.fromIDN(punycode);
       return Right(text);
     } on Exception catch (_) {
       return const Left(PunycodeConversionFailure.invalidPunycode);
