8410c911571230ed16b61f9912806bd3e27643f1
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2022-07-31 01:38:33 +0400

    chore: Remove unused import

--- lib/presentation/settings/settings_screen.dart
+++ lib/presentation/settings/settings_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_top_blocked_bouncing_scroll_physics/flutter_top_blocked_bouncing_scroll_physics.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
