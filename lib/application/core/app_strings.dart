e56de950740b07fd19e10ab705087b48b865e58e
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2022-08-02 22:24:47 +0400

    chore: Add app store ID

--- lib/application/core/app_strings.dart
+++ lib/application/core/app_strings.dart
@@ -1,6 +1,6 @@
 /// Holds static strings that don't require localization (e.g. URLs).
 abstract class AppStrings {
-  static const appStoreId = 'TODO: Add App Store ID';
+  static const appStoreId = '1637273151';
 
   static const supportEmail = 'support@fperson.dev';
   static const supportEmailLink = 'mailto:$supportEmail';
